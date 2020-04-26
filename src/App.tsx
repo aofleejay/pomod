aa1ec7030ef3e7d76e1d4ab768b1571fbe2af726
คุณาพจน์ ไพรัตน์ <aofleejay@hotmail.com>
2020-04-26 14:05:35 +0700

    :sparkles: Working pomodoro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+const SECOND = 1000
+const MINUTE = 60 * SECOND
+const BASE_WORKTIME = 25 * MINUTE
+
+const App: React.FC = () => {
+  const [timeLeft, setTimeLeft] = useState(BASE_WORKTIME)
+  let interval: NodeJS.Timeout | undefined
+
+  useEffect(() => {
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  }, [interval])
+
+  const countdown = () => {
+    setTimeLeft((prevTimeLeft) => {
+      if (prevTimeLeft - SECOND <= 0 && interval) {
+        clearInterval(interval)
+        return 0
+      }
+      return prevTimeLeft - SECOND
+    })
+  }
+
+  const onStartTimer = () => {
+    countdown()
+    interval = setInterval(countdown, SECOND)
+  }
+
+  const formatDigit = (digit: number): string => {
+    return digit.toString().padStart(2, '0')
+  }
+
+  const getTimeLeft = () => {
+    return {
+      minutes: Math.floor(timeLeft / MINUTE),
+      seconds: Math.floor((timeLeft % MINUTE) / SECOND),
+    }
+  }
+
+  const { minutes, seconds } = getTimeLeft()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{formatDigit(minutes)}</span>
+      <span>:</span>
+      <span>{formatDigit(seconds)}</span>
+      <button onClick={onStartTimer}>start</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
